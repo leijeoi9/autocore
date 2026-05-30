0be9a832a3c49338ededf5311943f3785835ced3
Lee <lee@example.com>
2026-05-30 11:32:12 +0800

    Week1 Day1: project skeleton and logger service

---
+++ build/app/can_service/CMakeFiles/can_service.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/can_service.dir/src/main.c.o"
+  "CMakeFiles/can_service.dir/src/main.c.o.d"
+  "can_service"
+  "can_service.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/can_service.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/app/can_service/CMakeFiles/can_service.dir/src/main.c.o.d
@@ -0,0 +1,4 @@
+app/can_service/CMakeFiles/can_service.dir/src/main.c.o: \
+ /home/leijeoi/autocore/app/can_service/src/main.c \
+ /usr/include/stdc-predef.h \
+ /home/leijeoi/autocore/service/logger/include/logger.h
