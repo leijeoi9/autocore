e2f9cda072751c2ae6125a5d9190964a748db1f5
Lee <lee@example.com>
2026-05-30 11:34:22 +0800

    add gitignore

--- build/app/can_service/CMakeFiles/can_service.dir/src/main.c.o.d
+++
@@ -1,4 +0,0 @@
-app/can_service/CMakeFiles/can_service.dir/src/main.c.o: \
- /home/leijeoi/autocore/app/can_service/src/main.c \
- /usr/include/stdc-predef.h \
- /home/leijeoi/autocore/service/logger/include/logger.h
